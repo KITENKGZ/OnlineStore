21ae8619cc68913105c3ef47a6cbe1976a25494d
Kamil Iminov <k.iminov98@gmail.com>
2024-08-24 13:33:56 +0800

    fixtures and debug toolbar added

--- Project/app/settings.py
+++ Project/app/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 
     'main',
     'goods',
@@ -39,6 +40,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -95,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'UTC'
 
@@ -117,3 +120,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
--- Project/goods/models.py
+++ Project/goods/models.py
@@ -1,3 +1,34 @@
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='カテゴリー名')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'カテゴリー'
+        verbose_name_plural = 'カテゴリー'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='商品名')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='商品説明')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='商品画像')
+    price = models.DecimalField(default=0, max_digits=7, decimal_places=0, verbose_name='商品価格')
+    discount = models.DecimalField(default=0, max_digits=7, decimal_places=0, verbose_name='割引％')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='在庫量')
+    category = models.ForeignKey(to=Categories, on_delete=models.PROTECT, verbose_name='カテゴリー')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = '商品'
+        verbose_name_plural = '商品'
+    
+    def __str__(self):
+        return f'{self.name} 在庫は{self.quantity}'
