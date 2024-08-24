780420eb817b9cf3a7495b1ec81dd898f45e8afb
Kamil Iminov <k.iminov98@gmail.com>
2024-08-25 00:58:31 +0800

    Simple search created

--- Project/goods/views.py
+++ Project/goods/views.py
@@ -1,17 +1,31 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
+from goods.utils import q_search
 from goods.models import Products
 
 # カタログを表示関数
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     # ページ番号を取得
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     # スラッグで商品をDBから取得、フィルタリング
-    if category_slug == "all":
+    if category_slug == 'all':
         goods = Products.objects.all()  # すべての商品を取得
+
+    elif query:
+        goods = q_search(query)
+        
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))  # 指定されたカテゴリーの商品を取得
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0) # セール中の商品を取得
+
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by) # 並び替え
 
     # ページネーション設定
     paginator = Paginator(goods, 3)
