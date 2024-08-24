00f0fce39f2865c2364910b386169124bf8d3f81
Kamil Iminov <k.iminov98@gmail.com>
2024-08-25 01:21:45 +0800

    Migrate db to postgresql

--- Project/app/settings.py
+++ Project/app/settings.py
@@ -72,8 +72,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
