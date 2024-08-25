14f2f4381c21d00fa4436f1846aee4c7c1b8ab4a
Kamil Iminov <k.iminov98@gmail.com>
2024-08-25 19:56:45 +0800

    Register, profile, notifications are done.

--- Project/users/views.py
+++ Project/users/views.py
@@ -1,9 +1,11 @@
-from django.contrib import auth
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
+from traitlets import Instance
 
-from users.forms import UserLoginForm
+from users.forms import ProfileForm, UserLoginForm, UserRegisterForm
 
 def login(request):
     if request.method == 'POST':
@@ -14,6 +16,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}様、ログイン成功")
                 return HttpResponseRedirect(reverse('main:index'))
 
 
@@ -27,16 +30,44 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            messages.success(request, f"{user.username}様、新規登録成功")
+            return HttpResponseRedirect(reverse('main:index'))
+
+    else:
+        form = UserRegisterForm()
+
     context = {
-        'title': 'ユーザー登録'
+        'title': 'ユーザー登録',
+        'form': form
     }
     return render(request, 'users/registration.html', context)
 
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "保存成功")
+            return HttpResponseRedirect(reverse('user:profile'))
+
+    else:
+        form = ProfileForm(instance=request.user)
+    
     context = {
-        'title': 'プロファイル'
+        'title': 'プロファイル',
+        'form': form
     }
     return render(request, 'users/profile.html', context)
 
+@login_required
 def logout(request):
-    pass
+    messages.success(request, "ログアウト成功")
+    auth.logout(request)
+    return redirect(reverse('main:index'))
